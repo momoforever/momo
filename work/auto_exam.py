46b2292f075a2b0ebfa31dd47664870c9047c2b3
momoforever <momoforever@vip.qq.com>
2016-05-19 21:44:29 +0800

    update auto_exam.py

--- work/auto_exam.py
+++ work/auto_exam.py
@@ -1,10 +1,32 @@
 # coding:utf-8
 
 import os
+import random
 import configparser
 
-with open('province.txt','r') as f:
-    province_data = f.readline()
+with open('province.txt', 'r') as f:
+    province_data = f.readlines()
 
-print(province_data)
+config = configparser.ConfigParser()
+with open('province_capital.txt', 'r') as cfg:
+    config.readfp(cfg)
+    provinces = config.sections()
+    province_capitals = {}
+    for i in range(provinces.__len__()):
+        province_capitals[provinces[i]] = config.get(provinces[i], 'city')
+    print(province_capitals)
 
+
+    #for i in range(config.__len__()):
+
+def generate_exam(n):
+    for i in range(1, n + 1):
+        province = provinces[random.randint(0, provinces.__len__())]
+        print('% s . 以下哪个城市是 % s 的省会？' % (i, province))
+        #print('A. % s' % )
+
+name = config.get('河北省', 'city')
+
+
+if __name__ == '__main__':
+    generate_exam(3)
